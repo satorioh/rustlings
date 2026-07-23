d7d930024bd2f5c9fa1000032e748284dcfab591
Robin.Wang <3Jg$94sbQ@q1>
2026-07-23 16:01:56 +0800

    feat(project): 11_hashmaps finished

--- solutions/11_hashmaps/hashmaps3.rs
+++ solutions/11_hashmaps/hashmaps3.rs
@@ -38,7 +38,7 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         team_2.goals_scored += team_2_score;
         team_2.goals_conceded += team_1_score;
     }
-    
+
     scores
 }
 
